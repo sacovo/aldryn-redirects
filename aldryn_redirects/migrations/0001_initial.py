4e905f457b34243aca06d9c1fd89a25ca4fd48e0
Sandro Covo <sandro@covo.ch>
2019-10-09 23:43:17 +0200

    Update 0001_initial.py

--- aldryn_redirects/migrations/0001_initial.py
+++ aldryn_redirects/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('old_path', models.CharField(help_text="This should be an absolute path, excluding the domain name. Example: '/events/search/'.", max_length=200, verbose_name='redirect from', db_index=True)),
-                ('site', models.ForeignKey(related_name='redirects_hvad_set', to='sites.Site')),
+                ('site', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='redirects_hvad_set', to='sites.Site')),
             ],
             options={
                 'ordering': ('old_path',),
